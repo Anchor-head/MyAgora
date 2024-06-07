96a6b16424c327780b78c8bb970c786f19c6d6c8
Anchor-head <vamphills@gmail.com>
2024-06-07 02:48:34 -0400

    push
    push

--- AgoraProject/AgoraApp/AIFunction/generateResponse.py
+++ AgoraProject/AgoraApp/AIFunction/generateResponse.py
@@ -1,3 +1,17 @@
 import openai
 
 
+token = 'sk-proj-OSCVrMnZJImNGyTymdzCT3BlbkFJiQrSu5gKottJTryihymU'
+def generateResponse(input_content):
+    client = openai.OpenAI(api_key=token)
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": input_content,
+            }
+        ],
+        model="gpt-4o",
+    )
+
+    return chat_completion.choices[0].message.content
--- AgoraProject/AgoraApp/tests.py
+++ AgoraProject/AgoraApp/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from .AduioToTextFunction.audioToText import transcribe_audio_file
+from .AIFunction.generateResponse import generateResponse
 import os
+from openai import OpenAI
 # Create your tests here.
 class ExampleTestCase(TestCase):
     def test_example(self):
@@ -8,8 +10,14 @@ class ExampleTestCase(TestCase):
         self.assertEqual(x,2)
     
 class AudioToTextConverterTestCase(TestCase):
-    def test_audioToTestExampleOne(self):
-        audio_path = os.path.join(os.path.dirname(__file__), '..', 'TestingResources', 'AudioTest1.wav')
-        transcribed_result = transcribe_audio_file(audio_path)
-        print(transcribed_result)
-        self.assertTrue(True)
+    # def test_audioToTestExampleOne(self):
+    #     audio_path = os.path.join(os.path.dirname(__file__), '..', 'TestingResources', 'AudioTest1.wav')
+    #     transcribed_result = transcribe_audio_file(audio_path)
+    #     print(transcribed_result)
+    #     self.assertTrue(True)
+
+    def test_openai_APIs(self):
+        response = generateResponse("when is the best time to fart?")
+        print(response)
+
+
